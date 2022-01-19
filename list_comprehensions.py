5e84cb5d144d12738f39a551fd1379b204705a9d
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-19 10:33:05 -0600

    Python Data Types Exercises - List Comprehensions (BONUS)

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -18,39 +18,79 @@ output = []
 for fruit in fruits:
     output.append(fruit.upper())
     
-# Exercise 1 - rewrite the above example code using list comprehension syntax. Make a variable named uppercased_fruits to hold the output of the list comprehension. Output should be ['MANGO', 'KIWI', etc...]
+# ---------------------------------------Exercise 1------------------------------------------------
+# Exercise 1 - rewrite the above example code using list comprehension syntax. Make a variable named uppercased_fruits 
+# to hold the output of the list comprehension. Output should be ['MANGO', 'KIWI', etc...]
+
+
+
+
+
+# ---------------------------------------Exercise 2------------------------------------------------
 
 # Exercise 2 - create a variable named capitalized_fruits and use list comprehension syntax to produce output like ['Mango', 'Kiwi', 'Strawberry', etc...]
 
+# ---------------------------------------Exercise 3------------------------------------------------
+
 # Exercise 3 - Use a list comprehension to make a variable named fruits_with_more_than_two_vowels. Hint: You'll need a way to check if something is a vowel.
+
+# ---------------------------------------Exercise 4------------------------------------------------
 
 # Exercise 4 - make a variable named fruits_with_only_two_vowels. The result should be ['mango', 'kiwi', 'strawberry']
 
+# ---------------------------------------Exercise 5------------------------------------------------
+
 # Exercise 5 - make a list that contains each fruit with more than 5 characters
+
+# ---------------------------------------Exercise 6------------------------------------------------
 
 # Exercise 6 - make a list that contains each fruit with exactly 5 characters
 
+# ---------------------------------------Exercise 7------------------------------------------------
+
 # Exercise 7 - Make a list that contains fruits that have less than 5 characters
+
+# ---------------------------------------Exercise 8------------------------------------------------
 
 # Exercise 8 - Make a list containing the number of characters in each fruit. Output would be [5, 4, 10, etc... ]
 
+# ---------------------------------------Exercise 9------------------------------------------------
+
 # Exercise 9 - Make a variable named fruits_with_letter_a that contains a list of only the fruits that contain the letter "a"
+
+# ---------------------------------------Exercise 10------------------------------------------------
 
 # Exercise 10 - Make a variable named even_numbers that holds only the even numbers 
 
+# ---------------------------------------Exercise 11------------------------------------------------
+
 # Exercise 11 - Make a variable named odd_numbers that holds only the odd numbers
+
+# ---------------------------------------Exercise 12------------------------------------------------
 
 # Exercise 12 - Make a variable named positive_numbers that holds only the positive numbers
 
+# ---------------------------------------Exercise 13------------------------------------------------
+
 # Exercise 13 - Make a variable named negative_numbers that holds only the negative numbers
+
+# ---------------------------------------Exercise 14------------------------------------------------
 
 # Exercise 14 - use a list comprehension w/ a conditional in order to produce a list of numbers with 2 or more numerals
 
+# ---------------------------------------Exercise 15------------------------------------------------
+
 # Exercise 15 - Make a variable named numbers_squared that contains the numbers list with each element squared. Output is [4, 9, 16, etc...]
+
+# ---------------------------------------Exercise 16------------------------------------------------
 
 # Exercise 16 - Make a variable named odd_negative_numbers that contains only the numbers that are both odd and negative.
 
+# ---------------------------------------Exercise 17------------------------------------------------
+
 # Exercise 17 - Make a variable named numbers_plus_5. In it, return a list containing each number plus five. 
+
+# ---------------------------------------Bonus------------------------------------------------
 
 # BONUS Make a variable named "primes" that is a list containing the prime numbers in the numbers list. *Hint* you may want to make or find a helper function that determines if a given number is prime or not.
 
