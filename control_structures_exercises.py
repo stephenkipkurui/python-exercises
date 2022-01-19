02dc2070c0e510f59f85109f4d7fba1c1e461692
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-19 11:55:53 -0600

    Python Control Structures Exercises

---
+++ control_structures_exercises.py
@@ -0,0 +1,10 @@
+# PYTHON CONTROL STRUCTURES
+
+# Conditional Basics
+# prompt the user for a day of the week, print out whether the day is Monday or not
+# prompt the user for a day of the week, print out whether the day is a weekday or a weekend
+# create variables and make up values for
+# the number of hours worked in one week
+# the hourly rate
+# how much the week's paycheck will be
+# write the python code that calculates the weekly paycheck. You get paid time and a half if you work more than 40 hours
